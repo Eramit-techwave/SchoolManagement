06072b877c2962e9f1eb39071cf3175344986073
Amit Dubey <amitdubey04102004@gmail.com>
2026-05-26 13:21:56 +0530

    Landing pages

--- frontend/src/pages/Landingpage.js
+++ frontend/src/pages/Landingpage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Landingpage.css';
 
 export default function Landingpage() {
-    const navigate = useNavigate();
 
     return (
         <div className="landing-page">
